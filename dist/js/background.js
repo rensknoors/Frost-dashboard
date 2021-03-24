c66fdba3230251920b4cc5e9c73ae4d6507f16f2
rensknoors <rensknoors@gmail.com>
2021-03-24 21:51:26 +0100

    Initialize new project setup

--- dist/js/background.js
+++
@@ -1,8 +0,0 @@
-// Script ran when the extension icon is clicked
-// Opens new tab (newtab.html)
-
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = "newtab.html";
-    chrome.tabs.create({ url: newURL });
-});
